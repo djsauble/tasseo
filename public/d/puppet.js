26712d09b78cc069e6e0f492887db90694810992
Daniel Sauble <djsauble@puppetlabs.com>
2012-05-17 10:13:41 -0700

    Adds sample puppet metrics

---
+++ public/d/puppet.js
@@ -0,0 +1,27 @@
+var metrics =
+[
+  {
+    "alias": "IRC Users",
+    "target": "irc.puppet.users",
+  },
+  {
+    "alias": "Twitter Followers",
+    "target": "twitter.followers_count",
+  },
+  {
+    "alias": "Facebook Likes",
+    "target": "facebook.likes",
+  },
+  {
+    "alias": "Job Postings",
+    "target": "linkedin.postings.puppet",
+  },
+  {
+    "alias": "Forge Modules",
+    "target": "forge.modules.total",
+  },
+  {
+    "alias": "Redmine Users",
+    "target": "redmine.users",
+  },
+];
